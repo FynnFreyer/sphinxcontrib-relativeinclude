64fa866beaf64a813ca73d49ff96283bb574b4ed
Fynn Freyer <fynn.freyer@googlemail.com>
2023-09-20 06:16:56 +0200

    docs: add boilerplate for doc generation

---
+++ docs/conf.py
@@ -0,0 +1,48 @@
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+from sphinxcontrib_relativeinclude.__about__ import __version__
+
+project = "sphinxcontrib-relativeinclude"
+author = "Fynn Freyer"
+copyright = "2023, Fynn Freyer"  # noqa: A001
+language = "en_US"
+
+version = __version__
+release = __version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinxcontrib.cairosvgconverter",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for output ------------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-latex-output
+
+html_theme = "classic"
+html_static_path = ["_static"]
+
+latex_elements = {
+    "extraclassoptions": "openany,oneside",
+    "papersize": "a4paper",
+    "pointsize": "12pt",
+    "figure_align": "H",
+}
+
+# -- Autodoc configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html
+
+add_module_names = False
+
+autodoc_mock_imports = ["docutils"]
+autodoc_member_order = "bysource"
---
+++ docs/index.rst
@@ -0,0 +1,12 @@
+Documentation for sphinxcontrib-relativeinclude
+===============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. include:: ../README.md
+   :parser: myst_parser.docutils_
+
+.. automodule:: sphinxcontrib_relativeinclude.relativeinclude
+    :members:
