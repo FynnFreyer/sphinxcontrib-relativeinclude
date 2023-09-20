dc90e6f0a54ac982add36629365c95190677cc19
Fynn Freyer <fynn.freyer@googlemail.com>
2023-09-20 06:14:14 +0200

    feat: make members available in module top level namespace

--- src/sphinxcontrib_relativeinclude/__init__.py
+++ src/sphinxcontrib_relativeinclude/__init__.py
@@ -1,3 +1,7 @@
 # SPDX-FileCopyrightText: 2023-present Fynn Freyer <fynn.freyer@googlemail.com>
 #
 # SPDX-License-Identifier: MIT
+
+from sphinxcontrib_relativeinclude.relativeinclude import LinkTranslator, RelativeInclude, setup
+
+__all__ = ["LinkTranslator", "RelativeInclude", "setup"]
