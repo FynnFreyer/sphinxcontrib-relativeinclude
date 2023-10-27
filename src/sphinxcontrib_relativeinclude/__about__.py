b62f01904ddea29ad8cde5838eb75f281c28fb0a
Fynn Freyer <fynn.freyer@googlemail.com>
2023-10-27 13:52:08 +0200

    docs: single source more information

--- src/sphinxcontrib_relativeinclude/__about__.py
+++ src/sphinxcontrib_relativeinclude/__about__.py
@@ -1,6 +1,15 @@
 # SPDX-FileCopyrightText: 2023-present Fynn Freyer <fynn.freyer@googlemail.com>
 #
 # SPDX-License-Identifier: MIT
-__author__ = "Fynn Freyer"
+
+__name__ = "sphinxcontrib-relativeinclude"
+__description__ = "Implements a new reST include directive to translate relative paths."
+
+__author_name__ = "Fynn Freyer"
 __author_email__ = "fynn.freyer@googlemail.com"
+
+__authors__ = {
+    {"name": __author_name__, "email": __author_email__},
+}
+
 __version__ = "0.0.1a1"
