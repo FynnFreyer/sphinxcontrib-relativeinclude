4228b017251f56dddad9eb58fdbe171b135bac02
Fynn Freyer <fynn.freyer@googlemail.com>
2023-10-29 13:16:09 +0100

    test: test import from namespace package works

--- tests/test_relativeinclude.py
+++ tests/test_relativeinclude.py
@@ -2,12 +2,15 @@
 #
 # SPDX-License-Identifier: MIT
 
+def test_can_import_from_namespace_package():
+    from sphinxcontrib.relativeinclude import RelativeInclude
+
 
 def test_resolve_one_indirection():
     raise NotImplementedError
 
 
-def test_resolve_multiple_indirectionn():
+def test_resolve_multiple_indirections():
     raise NotImplementedError
 
 
